a28cb026956690339f71295c9a886a88cb4ffdd6
arifthalhah <65509122+arifth@users.noreply.github.com>
2022-11-20 20:40:22 +0700

    commmit malam pildun \

--- Minggu_Pertama/Pengenalan_HTML/Day-4-passingDataToArrayandRenderIt/formhandle.js
+++ Minggu_Pertama/Pengenalan_HTML/Day-4-passingDataToArrayandRenderIt/formhandle.js
@@ -1,5 +1,22 @@
 
+let data = [{
+    name,
+    desc,
 
-function handleForm(){
-    console.log('halo cokk')
+}]
+
+function handleForm(event){
+    // event.preventDefault
+    var name = document.getElementById('name').value
+    var desc = document.getElementById('desc').value
+
+    // console.log( name, desc)
+
+    data.push({name:name,desc,desc})
+
+    for(index=0;index<data;data++){
+        console.log(data);
+    }    
+
+
 }
