f9d584abed5e61f7995812eab7ed6fe25b5cf6bd
arifthalhah <65509122+arifth@users.noreply.github.com>
2022-11-30 10:31:02 +0700

    organize folder (3)

--- Minggu_Kedua_Ketiga/Day9-ArrayManipulation/main.go
+++ Minggu_Kedua_Ketiga/Day9-ArrayManipulation/main.go
@@ -19,13 +19,24 @@ type Card struct {
 	End_date     string
 	Durasi       string
 	Desc         string
-	Tech         [4]string
-	Img          string
+	// handle checkbox with fix length array , not slices causing error when validating process in template
+	Tech [4]string
+	Img  string
 }
 
 // array dng index id dan isi interface cards
 
-var Cards = []Card{}
+var Cards = []Card{
+	{
+		Project_name: "Kursus Golang sampai Botak",
+		Start_date:   "11-10-2022",
+		End_date:     "11-11-2022",
+		Durasi:       "6 Bulan",
+		Desc:         "lorem10",
+		Tech:         [4]string{"nodejs", "java", "react", "ts"},
+		Img:          "",
+	},
+}
 
 // RENDER DATA DENGAN AWALAN HURUF KAPITAL
 
@@ -53,11 +64,20 @@ func main() {
 	route.HandleFunc("/", home).Methods("GET")
 	// contact page
 	route.HandleFunc("/contact", contact).Methods("GET")
+
 	// add Course
 	route.HandleFunc("/addproject", addProject).Methods("GET")
+	// update Course
+	route.HandleFunc("/updatecard/{id}", updatecard).Methods("GET")
+	// Delete Course
+	route.HandleFunc("/deletecard/{id}", deletecard).Methods("GET")
+
 	// api handle form value & redirect to /
 	route.HandleFunc("/sendform", sendform).Methods("POST")
-	Port := "5000"
+	// handle page detail card
+	route.HandleFunc("/detailcard/{id}", detailcard).Methods("GET")
+
+	Port := "5500"
 
 	fmt.Print("server running on port" + Port)
 
@@ -112,6 +132,54 @@ func addProject(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func updatecard(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hallo from updatecard")
+	tmpl, err := template.ParseFiles("public/updatecard.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("message : " + err.Error()))
+		return
+	} else {
+		var UpdateData = Card{}
+		// parse and assign index query url to variable
+		index, _ := strconv.Atoi(mux.Vars(r)["index"])
+
+		for i, data := range Cards {
+			if index == i {
+				UpdateData = Card{
+					Project_name: data.Project_name,
+					Start_date:   data.Start_date,
+					End_date:     data.End_date,
+					Durasi:       data.Durasi,
+					Desc:         data.Desc,
+					Tech:         data.Tech,
+					Img:          "",
+				}
+				// notice this
+				Cards = append(Cards[:index], Cards[index+1:]...)
+			}
+		}
+		data := map[string]interface{}{
+			"updateCard": UpdateData,
+		}
+		w.WriteHeader(http.StatusOK)
+		tmpl.Execute(w, data)
+	}
+
+}
+
+func deletecard(w http.ResponseWriter, r *http.Request) {
+	index, _ := strconv.Atoi(mux.Vars(r)["index"])
+
+	// ProjectList = append(ProjectList[:index], ProjectList[index+1:]...)
+	Cards = append(Cards[:index], Cards[index+1:]...)
+
+	fmt.Println(Cards)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+
+}
+
 func sendform(w http.ResponseWriter, r *http.Request) {
 
 	// fmt.Print("hallo form sendform")
@@ -149,6 +217,58 @@ func sendform(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 
 	}
+}
+
+func detailcard(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	template, err := template.ParseFiles("public/detailcard.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message:" + err.Error()))
+		// return kosong spy tidak eksekusi baris kode dibawahnya
+		return
+	} else {
+		// variable buat nampung isi detail cards
+		var Container = Card{}
+		// variable index parse id query ke string
+		index, _ := strconv.Atoi(mux.Vars(r)["id"])
+
+		for id, data := range Cards {
+			// cek apakah id valid atau tidak,still doesnt work -_-
+			if index != id {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			// cek apakah index sesuai query di URL
+			if index == id {
+				// masukkan data ke variabel penampung
+				Container = Card{
+					Project_name: data.Project_name,
+					Start_date:   data.Start_date,
+					End_date:     data.End_date,
+					Durasi:       data.Durasi,
+					Desc:         data.Desc,
+					Tech:         data.Tech,
+					Img:          "",
+				}
+			}
+		}
+
+		fmt.Println(Container.Tech)
+
+		// buat variable map dengan key string interface kosong utk data passing ke template
+
+		data := map[string]interface{}{
+			"detailCards": Container,
+		}
+
+		// ksh tau browser request berhasil
+		w.WriteHeader(http.StatusOK)
+		// eksekusi template
+		template.Execute(w, data)
+
+		// fmt.Println(id)
+	}
+
 }
 
 func getDuration(start_date string, end_date string) string {
